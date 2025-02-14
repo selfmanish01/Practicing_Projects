0a1597c2ba7ece13450555f44160baa6b4d2a1bc
selfmanish01 <selfmanish01@gmail.com>
2025-02-15 00:52:14 +0530

    enhance Todo app with loading state, toggle functionality, and improved UI

--- frontend/src/redux/App.js
+++ frontend/src/redux/App.js
@@ -1,30 +1,72 @@
-import { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchTodos, addTodo, updateTodo, deleteTodo } from "./redux/todoSlice";
-import { Button, TextField, List, ListItem, ListItemText } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTodos, addTodo, toggleTodo, deleteTodo } from "./redux/todoSlice";
+import { Container, TextField, Button, List, ListItem, ListItemText, IconButton, Checkbox, Typography, Paper } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function App() {
-    const [newTodo, setNewTodo] = useState("");
-    const todos = useSelector(state => state.todos);
     const dispatch = useDispatch();
+    const { todos, loading } = useSelector((state) => state.todos);
+    const [newTodo, setNewTodo] = useState("");
 
     useEffect(() => {
         dispatch(fetchTodos());
     }, [dispatch]);
 
+    const handleAddTodo = () => {
+        if (newTodo.trim() === "") return;
+        dispatch(addTodo(newTodo));
+        setNewTodo(""); // Clear input after adding
+    };
+
     return (
-        <div>
-            <TextField value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
-            <Button onClick={() => { dispatch(addTodo(newTodo)); setNewTodo(""); }}>Add</Button>
-            <List>
-                {todos.map((todo) => (
-                    <ListItem key={todo._id} onClick={() => dispatch(updateTodo({ id: todo._id, completed: !todo.completed }))}>
-                        <ListItemText primary={todo.title} secondary={todo.completed ? "Completed" : "Pending"} />
-                        <Button onClick={() => dispatch(deleteTodo(todo._id))}>Delete</Button>
-                    </ListItem>
-                ))}
-            </List>
-        </div>
+        <Container maxWidth="sm">
+            <Paper elevation={3} style={{ padding: "20px", marginTop: "20px" }}>
+                <Typography variant="h4" align="center" gutterBottom>
+                    TODO App
+                </Typography>
+
+                {/* Input Field and Add Button */}
+                <div style={{ display: "flex", marginBottom: "20px" }}>
+                    <TextField
+                        fullWidth
+                        variant="outlined"
+                        label="Add a new task"
+                        value={newTodo}
+                        onChange={(e) => setNewTodo(e.target.value)}
+                    />
+                    <Button variant="contained" color="primary" onClick={handleAddTodo} style={{ marginLeft: "10px" }}>
+                        Add
+                    </Button>
+                </div>
+
+                {/* Show Loading */}
+                {loading && <Typography align="center">Loading...</Typography>}
+
+                {/* Todo List */}
+                <List>
+                    {todos.map((todo) => (
+                        <ListItem key={todo._id} divider>
+                            <Checkbox
+                                checked={todo.completed}
+                                onChange={() => dispatch(toggleTodo(todo._id))}
+                            />
+                            <ListItemText
+                                primary={todo.text}
+                                style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+                            />
+                            <IconButton edge="end" color="secondary" onClick={() => {
+                                if (window.confirm("Are you sure you want to delete this task?")) {
+                                    dispatch(deleteTodo(todo._id));
+                                }
+                            }}>
+                                <DeleteIcon />
+                            </IconButton>
+                        </ListItem>
+                    ))}
+                </List>
+            </Paper>
+        </Container>
     );
 }
 
--- frontend/src/redux/todoSlice.js
+++ frontend/src/redux/todoSlice.js
@@ -1,41 +1,58 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-
-const API_URL = "http://localhost:5000/api/todos";
-
-export const fetchTodos = createAsyncThunk("todos/fetchTodos", async () => {
-    const response = await axios.get(API_URL);
-    return response.data;
-});
-
-export const addTodo = createAsyncThunk("todos/addTodo", async (title) => {
-    const response = await axios.post(API_URL, { title });
-    return response.data;
-});
-
-export const updateTodo = createAsyncThunk("todos/updateTodo", async ({ id, completed }) => {
-    const response = await axios.put(`${API_URL}/${id}`, { completed });
-    return response.data;
-});
-
-export const deleteTodo = createAsyncThunk("todos/deleteTodo", async (id) => {
-    await axios.delete(`${API_URL}/${id}`);
-    return id;
-});
-
 const todoSlice = createSlice({
     name: "todos",
-    initialState: [],
+    initialState: { todos: [], loading: false },
     reducers: {},
     extraReducers: (builder) => {
         builder
-            .addCase(fetchTodos.fulfilled, (state, action) => action.payload)
-            .addCase(addTodo.fulfilled, (state, action) => [...state, action.payload])
-            .addCase(updateTodo.fulfilled, (state, action) =>
-                state.map(todo => todo._id === action.payload._id ? action.payload : todo))
-            .addCase(deleteTodo.fulfilled, (state, action) =>
-                state.filter(todo => todo._id !== action.payload));
+            .addCase(fetchTodos.pending, (state) => { state.loading = true; })
+            .addCase(fetchTodos.fulfilled, (state, action) => {
+                state.loading = false;
+                state.todos = action.payload;
+            })
+            .addCase(fetchTodos.rejected, (state) => { state.loading = false; });
     },
 });
 
-export default todoSlice.reducer;
+
+
+// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import axios from "axios";
+
+// const API_URL = "http://localhost:5000/api/todos";
+
+// export const fetchTodos = createAsyncThunk("todos/fetchTodos", async () => {
+//     const response = await axios.get(API_URL);
+//     return response.data;
+// });
+
+// export const addTodo = createAsyncThunk("todos/addTodo", async (title) => {
+//     const response = await axios.post(API_URL, { title });
+//     return response.data;
+// });
+
+// export const updateTodo = createAsyncThunk("todos/updateTodo", async ({ id, completed }) => {
+//     const response = await axios.put(`${API_URL}/${id}`, { completed });
+//     return response.data;
+// });
+
+// export const deleteTodo = createAsyncThunk("todos/deleteTodo", async (id) => {
+//     await axios.delete(`${API_URL}/${id}`);
+//     return id;
+// });
+
+// const todoSlice = createSlice({
+//     name: "todos",
+//     initialState: [],
+//     reducers: {},
+//     extraReducers: (builder) => {
+//         builder
+//             .addCase(fetchTodos.fulfilled, (state, action) => action.payload)
+//             .addCase(addTodo.fulfilled, (state, action) => [...state, action.payload])
+//             .addCase(updateTodo.fulfilled, (state, action) =>
+//                 state.map(todo => todo._id === action.payload._id ? action.payload : todo))
+//             .addCase(deleteTodo.fulfilled, (state, action) =>
+//                 state.filter(todo => todo._id !== action.payload));
+//     },
+// });
+
+// export default todoSlice.reducer;
